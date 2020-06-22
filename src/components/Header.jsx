ab2c32ad5f911dbcf1b0ef813be8f69d6c58d3bd
Oleg Kabolov <afterouy@gmail.com>
2020-06-22 13:06:38 +0300

    разбил на компоненты, базовые блоки

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img src = 'https://brannetmarket.com/wp-content/uploads/2018/08/BRALOGO04.jpg'/>
+      </header>
+  } 
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className = 'main-content'>
+    <img src='https://vagabondtoursofireland.com/wp-content/uploads/2016/03/glendalough-reflection-river-tower.jpg?w1497'/>
+    <div class="avatar">ava + description</div>
+    <div class="my-posts">My Posts</div>
+    <div class="new-post">
+      <h2>New post</h2>
+      <p>Post	1</p>
+      <p>Post 2</p>
+    </div>
+  </div>
+  } 
+
+export default Profile;
